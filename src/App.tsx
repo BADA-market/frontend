30acdedf0339b3bf04ca8ece5a1921405b03af8a
kimsunjae <woal9844@gmail.com>
2023-12-20 17:02:45 +0900

    회원가입 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import TestPage from './pages/TestPage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TestPage />} />
+        <Route path="/sign_up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// interface LoginProps {}
+
+function LoginPage() {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  )
+}
+
+export default LoginPage
+
+const Container = styled.div`
+  width: 776px;
+  height: 970px;
+  border-radius: 20px;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
